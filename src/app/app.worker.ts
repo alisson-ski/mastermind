8440f9aacb73ac0601bc6c83c338e9367d3f1780
Alisson Lewinski <lissluc7@gmail.com>
2024-04-24 19:07:38 -0300

    add restart button + more styles

--- src/app/app.worker.ts
+++
@@ -1,9 +0,0 @@
-/// <reference lib="webworker" />
-
-addEventListener('message', ({ data }) => {
-  if (data === 'start') {
-    setInterval(() => {
-      postMessage('tick');
-    }, 1000);
-  }
-});
